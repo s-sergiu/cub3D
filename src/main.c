8cd1f7fc37b020b704163c6e00da28c0d82ea43e
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-04-09 16:58:36 +0200

    fixed double free

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 
 #include "../include/cub3D.h"
 
+void	free_split(char **str)
+{
+	int	i;	
+
+	i = -1;
+	while (str[++i])
+		free(str[i]);
+	free(str);
+}
+
 int	main(int argc, char **argv)
 {
 	char *map_string;
@@ -14,12 +24,14 @@ int	main(int argc, char **argv)
 	{
 		write(1, "success\n", 8);
 		map_string = read_map(argv[1]);
+		printf("map string: %s\n", map_string);
 		if (map_string)
 		{
 			map = ft_split(map_string, '\n');
 			get_width_and_height(map);
+			free_split(map);
 		}
+		free(map_string);
 	}
-	free(map);
 	return (0);
 }
