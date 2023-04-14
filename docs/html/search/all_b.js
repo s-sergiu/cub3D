45618ee2137ea14f80df0cdb5583597fb420fb42
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-04-15 00:58:16 +0200

    Updated docs.

--- docs/html/search/all_b.js
+++ docs/html/search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['read_5fmap_0',['read_map',['../cub3D_8h.html#a4262bf4509b96716171e25f0f9f0644e',1,'read_map(char *file):&#160;read_map.c'],['../read__map_8c.html#a4262bf4509b96716171e25f0f9f0644e',1,'read_map(char *file):&#160;read_map.c']]],
+  ['read_5fmap_0',['read_map',['../cub3D_8h.html#a953ff0338bfa5e5863fc98056025e303',1,'read_map(char *filename):&#160;read_map.c'],['../read__map_8c.html#a953ff0338bfa5e5863fc98056025e303',1,'read_map(char *filename):&#160;read_map.c']]],
   ['read_5fmap_2ec_1',['read_map.c',['../read__map_8c.html',1,'']]],
   ['readme_2emd_2',['README.md',['../README_8md.html',1,'']]]
 ];
--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cleanup_5fand_5fexit_0',['cleanup_and_exit',['../cub3D_8h.html#aabc8b9c956e025066b300e71e80f2590',1,'cleanup_and_exit(int error_number, int fd, char *buffer):&#160;error_handling.c'],['../error__handling_8c.html#aabc8b9c956e025066b300e71e80f2590',1,'cleanup_and_exit(int error_number, int fd, char *buffer):&#160;error_handling.c']]]
+  ['cleanup_5fand_5fexit_0',['cleanup_and_exit',['../cub3D_8h.html#a7584d3cba7faed7cee5d3d2120f43e83',1,'cleanup_and_exit(int error_number, int file, char *buffer):&#160;error_handling.c'],['../error__handling_8c.html#a7584d3cba7faed7cee5d3d2120f43e83',1,'cleanup_and_exit(int error_number, int file, char *buffer):&#160;error_handling.c']]]
 ];
