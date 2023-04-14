17a1b2711a7b9aefedf889608b19574f98a66d18
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-04-15 00:57:46 +0200

    Removed filename_is_valid function (redundant).

--- src/error_handling.c
+++ src/error_handling.c
@@ -24,22 +24,12 @@ int	handle_error(int error_number)
 	exit(error_number);
 }
 
-void	cleanup_and_exit(int error_number, int fd, char *buffer)
+void	cleanup_and_exit(int error_number, int file, char *buffer)
 {
-	if (fd > 0)
-		close(fd);
+	if (file > 0)
+		close(file);
 	if (buffer != NULL)
 		free(buffer);
 	print_error(error_number);
 	exit(error_number);
-}
-
-int	filename_is_valid(char *filename)
-{
-	int	filename_length;
-
-	filename_length = ft_strlen(filename);
-	if (ft_strncmp(filename + (filename_length - 4), ".cub\0", 5))
-		handle_error(BAD_MAP_FILE);
-	return (TRUE);
 }
