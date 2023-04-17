b8a27ecc5370c1c0198dda99bd2bfda5c9ba7139
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-04-17 06:44:42 +0200

    Added CMakeLists for libft and main project. Implemented simple google test unit test in tests dir.

---
+++ tests/hello_test.cc
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "include/cub3D.h"
+}
+// Demonstrate some basic assertions.
+TEST(Sergiu, TestFilename) {
+  // Expect equality.
+  EXPECT_DEATH( read_map(0) , "Error:*");
+  //EXPECT_EXIT( read_map(0) , ::testing::ExitedWithCode(14), "Error:*");
+}
