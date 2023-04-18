2f8f76eb49c4fc84376495c07ec2902c13cb6077
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-04-18 18:14:46 +0200

    Added 2 maps for testing. Added extra tests.

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -4,8 +4,25 @@ extern "C" {
 #include "../include/cub3D.h"
 }
 // Demonstrate some basic assertions.
-TEST(Sergiu, TestFilename) {
-  // Expect equality.
-  EXPECT_DEATH( read_map(0) , "Error:*");
-  //EXPECT_EXIT( read_map(0) , ::testing::ExitedWithCode(14), "Error:*");
+TEST(read_map, zero_number) {
+  EXPECT_EXIT( read_map(0) , ::testing::ExitedWithCode(14), "Error:*");
+}
+
+TEST(read_map, NULL) {
+  EXPECT_EXIT( read_map(NULL) , ::testing::ExitedWithCode(14), "Error:*");
+}
+
+TEST(read_map, normal_map) {
+  char filename[] = "../data/maps/test1.cub";
+  EXPECT_STREQ(read_map(filename), "maptest1\n");
+}
+
+TEST(read_map, no_extension) {
+  char filename[] = "../data/maps/test2";
+  EXPECT_STREQ(read_map(filename), "noextension\n");
+}
+
+TEST(getTotalBytes, filename) {
+  char filename[] = "../data/maps/test2";
+  EXPECT_EQ(get_total_bytes(filename), 12);
 }
