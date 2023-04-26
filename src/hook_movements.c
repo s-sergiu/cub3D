32bc183fd508ff5a73654afe87f49e08232a6c45
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-04-26 17:28:05 +0200

    Player pixel now collides properly with all walls.

--- src/hook_movements.c
+++ src/hook_movements.c
@@ -21,7 +21,7 @@ void	turn_left(char **map, t_game *game_data)
 	mlx_image_t			*player;
 
 	player = game_data->player_data.player_image;
-	posx = (player->instances[0].x - 1)/ TILE;
+	posx = (player->instances[0].x - 2)/ TILE;
 	posy = (player->instances[0].y)/ TILE;
 	if (map[posy][posx] != '1') 
 		player->instances[0].x -= 1;
@@ -48,7 +48,7 @@ void	turn_up(char **map, t_game *game_data)
 
 	player = game_data->player_data.player_image;
 	posx = (player->instances[0].x)/ TILE;
-	posy = (player->instances[0].y - 1)/ TILE;
+	posy = (player->instances[0].y - 2)/ TILE;
 	if (map[posy][posx] != '1') 
 		player->instances[0].y -= 1;
 }
