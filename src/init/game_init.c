06d4a40477c2ba03da6fc61b1b446d54bb166d81
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-05-26 01:37:26 +0200

    draw 1 wall

--- src/init/game_init.c
+++ src/init/game_init.c
@@ -26,14 +26,10 @@ void	destroy_game_data(t_game *game_data)
 void	init_game_data(t_game **game_data, t_map *map_data)
 {
 	mlx_t			*mlx;
-	int				width;
-	int				height;
 
-	width = map_data->width;
-	height = map_data->height;
 	mlx = NULL;
 	malloc_game_struct(game_data, *destroy_map_data, map_data);
-	init_mlx(&mlx, width, height);
+	init_mlx(&mlx, 1920, 1080);
 	if (!mlx)
 	{
 		destroy_game_data(*game_data);
@@ -42,6 +38,10 @@ void	init_game_data(t_game **game_data, t_map *map_data)
 	}
 	(*game_data)->mlx = mlx;
 	(*game_data)->bg_img = NULL;
+	(*game_data)->wall = NULL;
+	(*game_data)->game_screen = NULL;
+	(*game_data)->floor = NULL;
+	(*game_data)->ceiling = NULL;
 	(*game_data)->map_data = map_data;
 	(*game_data)->player_data.player_image = NULL;
 	(*game_data)->player_data.angle = 3 * M_PI_2;
