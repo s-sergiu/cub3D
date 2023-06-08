cf857a1d0d6236776c8fd3a2f08d4bc69b23ac91
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-06-09 00:49:52 +0200

    update

--- src/draw_map_utils.c
+++ src/draw_map_utils.c
@@ -46,64 +46,64 @@ int	get_color_texture(mlx_texture_t *tex, t_game *game_data, int topy)
 	if ((int)y % TILE == 0)
 	{
 		if (((fov_end >= M_PI) || fov_start >= M_PI)
-			|| ((fov_end <= M_PI * 2 || fov_start >= 0)
-				|| (fov_start < M_PI / 6)))
+				|| ((fov_end <= M_PI * 2 || fov_start >= 0)
+					|| (fov_start < M_PI / 6)))
 		{
-		texture_y = (topy) * ((double)north->height - 1) / height;
-		texture_x = fmod((x / TILE), 1.0) * north->width;
-		position = (texture_y * north->width + texture_x)
-			* north->bytes_per_pixel;
-		color = get_rgba(north->pixels[position],
-				north->pixels[position + 1], north->pixels[position + 2],
-				north->pixels[position + 3]);
-		return (color);
+			texture_y = (topy) * ((double)north->height - 1) / height;
+			texture_x = fmod((x / TILE), 1.0) * north->width;
+			position = (texture_y * north->width + texture_x)
+				* north->bytes_per_pixel;
+			color = get_rgba(north->pixels[position],
+					north->pixels[position + 1], north->pixels[position + 2],
+					north->pixels[position + 3]);
+			return (color);
 		}
 	}
-	   if ((int)x % TILE == 0)
-	   {
-	   if ((fov_end < 3 * M_PI_2) || (fov_start > M_PI_2))
-	   {
-	   texture_y = (topy) * ((double)west->height - 1) / height;
-	   texture_x = fmod((y / TILE), 1.0) * west->width;
-	   position = (texture_y * west->width + texture_x)
-	 * west->bytes_per_pixel;
-	 color = get_rgba(west->pixels[position],
-	 west->pixels[position + 1], west->pixels[position + 2],
-	 west->pixels[position + 3]);
-	 return (color);
-	 }
-	 }
-	 if ((int)y % TILE == 1)
-	 {
-	 if ((fov_end <= 3 * M_PI_2) || (fov_start >= 3 * M_PI_2)
-	 || (fov_end >= 0) || ((fov_start >= 0)
-	 && (fov_start > 11 * M_PI / 6)))
-	 {
-	 texture_y = (topy) * ((double)south->height - 1) / height;
-	 texture_x = fmod((x / TILE), 1.0) * south->width;
-	 position = ((texture_y) * south->width
-	 + (south->width - 1 - texture_x)) * south->bytes_per_pixel;
-	 color = get_rgba(south->pixels[position],
-	 south->pixels[position + 1], south->pixels[position + 2],
-	 south->pixels[position + 3]);
-	 return (color);
-	 }
-	 }
-	 if ((int)x % TILE == 1)
-	 {
-	 if ((fov_start > 3 * M_PI_2 || fov_start < 2 * M_PI)
-	 || (fov_end < M_PI_2))
-	 {
-	 texture_y = (topy) * ((double)east->height - 1) / height;
-	 texture_x = fmod((y / TILE), 1.0) * east->width;
-	 position = (texture_y * east->width + texture_x)
-	 * east->bytes_per_pixel;
-	 color = get_rgba(east->pixels[position],
-	 east->pixels[position + 1], east->pixels[position + 2],
-	 east->pixels[position + 3]);
-	 return (color);
-	 }
-	 }
+	if ((int)x % TILE == 0)
+	{
+		if ((fov_end < 3 * M_PI_2) || (fov_start > M_PI_2))
+		{
+			texture_y = (topy) * ((double)west->height - 1) / height;
+			texture_x = fmod((y / TILE), 1.0) * west->width;
+			position = (texture_y * west->width + texture_x)
+				* west->bytes_per_pixel;
+			color = get_rgba(west->pixels[position],
+					west->pixels[position + 1], west->pixels[position + 2],
+					west->pixels[position + 3]);
+			return (color);
+		}
+	}
+	if ((int)y % TILE == 1)
+	{
+		if ((fov_end <= 3 * M_PI_2) || (fov_start >= 3 * M_PI_2)
+				|| (fov_end >= 0) || ((fov_start >= 0)
+					&& (fov_start > 11 * M_PI / 6)))
+		{
+			texture_y = (topy) * ((double)south->height - 1) / height;
+			texture_x = fmod((x / TILE), 1.0) * south->width;
+			position = ((texture_y) * south->width
+					+ (south->width - 1 - texture_x)) * south->bytes_per_pixel;
+			color = get_rgba(south->pixels[position],
+					south->pixels[position + 1], south->pixels[position + 2],
+					south->pixels[position + 3]);
+			return (color);
+		}
+	}
+	if ((int)x % TILE == 1)
+	{
+		if ((fov_start > 3 * M_PI_2 || fov_start < 2 * M_PI)
+				|| (fov_end < M_PI_2))
+		{
+			texture_y = (topy) * ((double)east->height - 1) / height;
+			texture_x = fmod((y / TILE), 1.0) * east->width;
+			position = (texture_y * east->width + texture_x)
+				* east->bytes_per_pixel;
+			color = get_rgba(east->pixels[position],
+					east->pixels[position + 1], east->pixels[position + 2],
+					east->pixels[position + 3]);
+			return (color);
+		}
+	}
 	return (color);
 }
 
@@ -152,64 +152,64 @@ void	player_angle(t_game **data, char orientation)
 		(*data)->player_data.angle = 3 * M_PI_2;
 }
 
-   void line_draw(t_vector *player, double angle, t_game *game_data)
-   {
-   t_vector d;
-   t_vector ray_direction;
-   t_int_vector map;
-   t_vector side_distance;
-   t_vector intersection;
-   int hit;
-   double distance;
+void line_draw(t_vector *player, double angle, t_game **game_data)
+{
+	t_vector d;
+	t_vector ray_direction;
+	t_int_vector map;
+	t_vector side_distance;
+	t_vector intersection;
+	int hit;
+	double distance;
 
-   ray_direction.x = cos(angle);
-   ray_direction.y = -sin(angle);
-   player[0] = (t_vector) {game_data->player_data.player_image->instances[0].x, game_data->player_data.player_image->instances[0].y};
-   map = (t_int_vector){ player[0].x/ TILE, player[0].y / TILE};
-   if (ray_direction.x > 0)
-   side_distance.x = ((map.x + 1) * TILE - player[0].x) / ray_direction.x;
-   else if (ray_direction.x < 0)
-   side_distance.x = ((map.x * TILE - player[0].x) / ray_direction.x);
-   if (ray_direction.y > 0)
-   side_distance.y = ((map.y + 1) * TILE - player[0].y) / ray_direction.y;
-   else if (ray_direction.y < 0)
-   side_distance.y = (map.y * TILE - player[0].y) / ray_direction.y;
-   d.x = fabs(TILE / ray_direction.x);
-   d.y = fabs(TILE / ray_direction.y);
-   hit = 0;
-   while (game_data->map_data->map_array[map.y][map.x] == '0')
-   {
-   if (side_distance.x < side_distance.y)
-   {
-   side_distance.x += d.x;
-   map.x += (ray_direction.x > 0) - (ray_direction.x < 0);
-   if (ray_direction.x > 0)
-   game_data->hit = EAST;
-   else
-   game_data->hit = WEST;
-   }
-   else
-   {
-   side_distance.y += d.y;
-   map.y = (ray_direction.y > 0) - (ray_direction.y <0);
-   if (ray_direction.y > 0)
-   game_data->hit = NORTH;
-   else
-   game_data->hit = SOUTH;
-   }
-   }
-   if (hit == EAST || hit == WEST) {
-   intersection.x = map.x * TILE + (ray_direction.x > 0 ? 0 : TILE);
-   intersection.y = player[0].y + (intersection.x - player[0].x) * tan(angle);
-   } else {
-   intersection.y = map.y * TILE + (ray_direction.y > 0 ? 0 : TILE);
-   intersection.x = player[0].x + (intersection.y - player[0].y) / tan(angle);
-   distance = sqrt(pow(intersection.x - player[0].x, 2) + pow(intersection.y - player[0].y, 2));
-   game_data->distance = distance;
-   game_data->player_data.end_ray.x_axis = intersection.x;
-   game_data->player_data.end_ray.y_axis = intersection.y;
-   }
-   }
+	ray_direction.x = cos(angle);
+	ray_direction.y = -sin(angle);
+	player[0] = (t_vector) {(*game_data)->player_data.player_image->instances[0].x, (*game_data)->player_data.player_image->instances[0].y};
+	map = (t_int_vector){ player[0].x/ TILE, player[0].y / TILE};
+	if (ray_direction.x > 0)
+		side_distance.x = ((map.x + 1) * TILE - player[0].x) / ray_direction.x;
+	else if (ray_direction.x < 0)
+		side_distance.x = ((map.x * TILE - player[0].x) / ray_direction.x);
+	if (ray_direction.y > 0)
+		side_distance.y = ((map.y + 1) * TILE - player[0].y) / ray_direction.y;
+	else if (ray_direction.y < 0)
+		side_distance.y = (map.y * TILE - player[0].y) / ray_direction.y;
+	d.x = fabs(TILE / ray_direction.x);
+	d.y = fabs(TILE / ray_direction.y);
+	hit = 0;
+	while ((*game_data)->map_data->map_array[map.y][map.x] == '0')
+	{
+		if (side_distance.x < side_distance.y)
+		{
+			side_distance.x += d.x;
+			map.x += (ray_direction.x > 0) - (ray_direction.x < 0);
+			if (ray_direction.x > 0)
+				(*game_data)->hit = EAST;
+			else
+				(*game_data)->hit = WEST;
+		}
+		else
+		{
+			side_distance.y += d.y;
+			map.y = (ray_direction.y > 0) - (ray_direction.y <0);
+			if (ray_direction.y > 0)
+				(*game_data)->hit = NORTH;
+			else
+				(*game_data)->hit = SOUTH;
+		}
+	}
+	if (hit == EAST || hit == WEST) {
+		intersection.x = map.x * TILE + (ray_direction.x > 0 ? 0 : TILE);
+		intersection.y = player[0].y + (intersection.x - player[0].x) * tan(angle);
+	} else {
+		intersection.y = map.y * TILE + (ray_direction.y > 0 ? 0 : TILE);
+		intersection.x = player[0].x + (intersection.y - player[0].y) / tan(angle);
+	}
+	distance = sqrt(pow(intersection.x - player[0].x, 2) + pow(intersection.y - player[0].y, 2));
+	(*game_data)->distance = distance;
+	(*game_data)->player_data.end_ray.x_axis = intersection.x;
+	(*game_data)->player_data.end_ray.y_axis = intersection.y;
+}
 
 
 void	line_draw2(t_vector *pointA, struct s_position *pointB,
@@ -278,7 +278,7 @@ void	draw_fov(t_game **game_data)
 	while (fov_angle < (*game_data)->player_data.angle + M_PI / 6)
 	{
 		update_end(game_data, fov_angle);
-		line_draw(origin, fov_angle, (*game_data));
+		line_draw(origin, fov_angle, game_data);
 		//line_draw2(origin, point, (*game_data)->map_data->map, (*game_data));
 		draw_wall(game_data, center, fov_shit);
 		fov_angle += DELTA_FOV;
